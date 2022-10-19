7b813156c70ef558fa8f91a1b9633aa3721115d5
Vincorp21 <vibarcar@gmail.com>
2022-10-19 14:23:38 +0200

    Delete files

--- 8kyu/remove-exclamation-marks.js
+++
@@ -1,8 +0,0 @@
-// DESCRIPTION:
-// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
-
-//My solution...
-
-function removeExclamationMarks(s) {
-  return s.replace('!', '').replace('!', '').replace('!', '');
-}
